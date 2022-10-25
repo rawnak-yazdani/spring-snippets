5d3078a9f13c0807722feb7a11acf9eeefb3f0b3
rawnak-yazdani <rawnakyazdani@gmail.com>
2022-10-25 16:39:37 +0600

    Added doc comments

--- src/main/java/io/welldev/Main.java
+++ src/main/java/io/welldev/Main.java
@@ -1,5 +1,31 @@
-package io.welldev;public class Main {
+package io.welldev;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         *  AbstractApplicationContext is spring container, and it is using Beans.xml (configuration metadata)
+         */
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        /**
+         * Object is created by the spring container
+         */
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld objB = (HelloWorld) context.getBean("helloWorld2");
+
+        objA.getMessage();
+        objB.getMessage();
+        context.registerShutdownHook();     // it will not work with ApplicationContext
+
+//        objA.setMessage("I'm Object A");
+//        objA.getMessage();
+//
+//        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+//        objB.getMessage();
     }
 }
