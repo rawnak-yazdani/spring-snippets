4fe71164092470a3c90d35cffc4f52121346761d
rawnak-yazdani <rawnakyazdani@gmail.com>
2022-10-26 15:22:50 +0600

    update

--- src/main/java/io/welldev/Main.java
+++ src/main/java/io/welldev/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         /**
          *  AbstractApplicationContext is spring container, and it is using Beans.xml (configuration metadata)
-         *  Container will create the beans/objects whether getBean() is used or not
+         *  Container will create the singleton beans/objects whether container.getBean() is used or not
+         *  But it will create prototype beans/objects only when container.getBean() is called.
          */
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 //        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
@@ -17,10 +18,10 @@ public class Main {
          * Object is created by the spring container
          */
         HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-        HelloWorld objB = (HelloWorld) context.getBean("helloWorld2");
+//        HelloWorld objB = (HelloWorld) context.getBean("helloWorld2");
 
         objA.getMessage();
-        objB.getMessage();
+//        objB.getMessage();
         context.registerShutdownHook();     // it will not work with ApplicationContext
 
 //        objA.setMessage("I'm Object A");
