20816c9ee00b786bfb3cac82e72064ca4efdb997
rawnak-yazdani <rawnakyazdani@gmail.com>
2022-10-30 13:03:27 +0600

    update

--- src/main/java/io/welldev/configuration/beans/Accountant.java
+++ src/main/java/io/welldev/configuration/beans/Accountant.java
@@ -1,4 +1,4 @@
-package io.welldev.beans;
+package io.welldev.configuration.beans;
 
 import org.springframework.stereotype.Component;
 
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
  * @Component is used to denote a bean/object
  */
 @Component
-public class Manager implements Employee{
+public class Accountant implements Employee{
     public void doWork() {
-        System.out.println("Manage the branch office");
+        System.out.println("Auditing Accounts...");
     }
 }
