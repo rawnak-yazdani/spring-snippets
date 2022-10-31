5f4b34e56b641b29f510b052378fa7793c859df9
rawnak-yazdani <rawnakyazdani@gmail.com>
2022-10-31 15:07:49 +0600

    update

--- src/main/java/io/welldev/Main.java
+++ src/main/java/io/welldev/Main.java
@@ -3,6 +3,11 @@ package io.welldev;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,22 +17,31 @@ public class Main {
          *  But it will create prototype beans/objects only when springContainer.getBean() is called.
          */
         AbstractApplicationContext springContainer = new ClassPathXmlApplicationContext("Beans.xml");
-//        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         /**
          * Object is created by the spring container and inversion of control is occurring (not DI)
          */
         HelloWorld objA = (HelloWorld) springContainer.getBean("helloWorld");
-//        HelloWorld objB = (HelloWorld) context.getBean("helloWorld2");
 
         objA.getMessage();
-//        objB.getMessage();
-        springContainer.registerShutdownHook();     // it will not work with ApplicationContext
 
-//        objA.setMessage("I'm Object A");
-//        objA.getMessage();
-//
-//        HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-//        objB.getMessage();
+//        Resource resource = springContainer.getResource("file:/Users/rawnakyazdani/Desktop/demo.txt");
+        Resource resource = springContainer.getResource("demo.txt");
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+
+            while (true) {
+                String s = bufferedReader.readLine();
+                if (s == null)
+                    break;
+                System.out.println(s);
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        springContainer.registerShutdownHook();     // it will not work with ApplicationContext
     }
 }
