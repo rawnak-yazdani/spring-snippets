8f82d6f11e6da4c4dd09e15f74137dc978491400
rawnak-yazdani <rawnakyazdani@gmail.com>
2022-11-07 14:43:40 +0600

    update

--- src/main/java/io/welldev/configuration/ConfigurationMetadata.java
+++ src/main/java/io/welldev/configuration/ConfigurationMetadata.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("io.welldev.configuration.beans")
 public class ConfigurationMetadata {
+    /**
+     * @Bean method name will be the bean/object name
+     * @Bean can create bean/object of a class which is outside the scope of @ComponentScan
+     */
     @Bean
     public Waiter waiteRRR() {
         return new Waiter();
