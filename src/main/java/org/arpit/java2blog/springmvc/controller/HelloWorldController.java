e794e1d7437e0d5f976662a3c7c6fa2c8f8dc8d5
rawnak-yazdani <rawnakasif@gmail.com>
2022-11-12 17:43:52 +0600

    updateFromLinux

--- src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
+++ src/main/java/org/arpit/java2blog/springmvc/controller/HelloWorldController.java
@@ -1,13 +1,15 @@
 package org.arpit.java2blog.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/helloworld")
+//    @RequestMapping("/helloworld")
+    @GetMapping("/helloworld")
     public ModelAndView hello() {
 
         String helloWorldMessage = "Hello world from java2blog!";
