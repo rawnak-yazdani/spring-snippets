13ba1395c2219b8842422b750b83c4d65d4db47c
rawnak-yazdani <rawnakasif@gmail.com>
2022-11-13 11:36:06 +0600

    updateFromMac

--- src/main/java/io/welldev/controller/HomeController.java
+++ src/main/java/io/welldev/controller/HomeController.java
@@ -1,13 +1,16 @@
 package io.welldev.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Locale;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/robbii")
-    public String getHome() {
+    public String getHome(Locale locale, Model model) {
         return "home";
     }
 }
