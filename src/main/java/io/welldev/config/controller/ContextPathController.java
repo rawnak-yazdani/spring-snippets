c0bf072cfa21f45b0094d1d1e03547d760775b5c
rawnak-yazdani <rawnakasif@gmail.com>
2022-11-17 15:57:47 +0600

    updateFromMac

--- src/main/java/io/welldev/config/controller/ContextPathController.java
+++ src/main/java/io/welldev/config/controller/ContextPathController.java
@@ -1,18 +1,11 @@
 package io.welldev.config.controller;
 
-import io.welldev.config.WebMvcConfig;
-import io.welldev.config.beans.StudentDaoImpl;
-import io.welldev.dataclass.Student;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
 @Controller
-public class AnotherController {
+public class ContextPathController {
     /**
      * Scope of ModelAndView is current request
      */
