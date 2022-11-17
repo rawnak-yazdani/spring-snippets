84364b725500899778058e091275090c54fd50be
rawnak-yazdani <rawnakasif@gmail.com>
2022-11-17 15:42:14 +0600

    updateFromMac

--- src/main/java/io/welldev/config/WebMvcConfig.java
+++ src/main/java/io/welldev/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package io.welldev.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,5 +20,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Bean("dataSource")
+    public DriverManagerDataSource getDataSource() {
+        DriverManagerDataSource dmd = new DriverManagerDataSource();
+        dmd.setDriverClassName("org.postgresql.Driver");
+        dmd.setUrl("jdbc:postgresql://localhost:5432/myfirstdb");
+        dmd.setUsername("rawnakyazdani");
+        dmd.setPassword("2222"); //here you set the password of your user
+
+        return dmd;
     }
 }
