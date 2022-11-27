2f8da66a86fef306551895fd72b32cfc6596fd49
rawnak-yazdani <rawnakasif@gmail.com>
2022-11-27 10:28:41 +0600

    UpdateFromMac

--- src/main/java/io/welldev/Main.java
+++ src/main/java/io/welldev/Main.java
@@ -15,63 +15,63 @@ public class Main {
         IlluminatiDAOTransactionScoped illuminatiDAOTransactionScoped = context.getBean(IlluminatiDAOTransactionScoped.class);
         IlluminatiDAOExtendedScoped illuminatiDAOExtendedScoped = context.getBean(IlluminatiDAOExtendedScoped.class);
 
-//        insertData_TransactionScoped_TransactionalMethod(illuminatiDAOTransactionScoped);
 //        insertData_TransactionScoped_NonTransactionalMethod(illuminatiDAOTransactionScoped);
+//        insertData_TransactionScoped_TransactionalMethod(illuminatiDAOTransactionScoped);
 
 //
+//        insertData_ExtendedScoped_NonTransactionalMethod(illuminatiDAOExtendedScoped);
 //        insertData_ExtencedScoped_TransactionalMethod(illuminatiDAOExtendedScoped);
-        insertData_ExtendedScoped_NonTransactionalMethod(illuminatiDAOExtendedScoped);
 
-//        listAllIlluminati(illuminatiDAOTransactionScoped);
+        listAllIlluminati(illuminatiDAOTransactionScoped);
 
     }
 
     private static void insertData_ExtendedScoped_NonTransactionalMethod(IlluminatiDAOExtendedScoped illuminatiDAO_ExtendedScoped) {
         Illuminati illuminati = new Illuminati();
-        illuminati.setName("Bill Gates 4");
-        illuminati.setCountry("USA");
+        illuminati.setName("Mukesh Ambani");
+        illuminati.setCountry("India");
         illuminati.setDesignation("ExtendedScoped_NonTransactional");
-        illuminati.setBalance(300000000);
+        illuminati.setBalance(200000000);
         illuminatiDAO_ExtendedScoped.addIlluminati_NonTransactional(illuminati);
-        System.out.println("----------------------------------------------------------");
-        System.out.println(illuminati.getName() + " has joined US!! ");
-        System.out.println("----------------------------------------------------------");
+//        System.out.println("----------------------------------------------------------");
+//        System.out.println(illuminati.getName() + " has joined US!! ");
+//        System.out.println("----------------------------------------------------------");
     }
 
     private static void insertData_ExtencedScoped_TransactionalMethod(IlluminatiDAOExtendedScoped illuminatiDAO_ExtendedScoped) {
         Illuminati illuminati = new Illuminati();
-        illuminati.setName("Bill Gates 3");
+        illuminati.setName("Elon Musk");
         illuminati.setCountry("USA");
         illuminati.setDesignation("ExtencedScoped_Transactional");
         illuminati.setBalance(300000000);
         illuminatiDAO_ExtendedScoped.addIlluminati_Transactional(illuminati);
-        System.out.println("----------------------------------------------------------");
-        System.out.println(illuminati.getName() + " has joined US!! ");
-        System.out.println("----------------------------------------------------------");
+//        System.out.println("----------------------------------------------------------");
+//        System.out.println(illuminati.getName() + " has joined US!! ");
+//        System.out.println("----------------------------------------------------------");
     }
 
     private static void insertData_TransactionScoped_NonTransactionalMethod(IlluminatiDAOTransactionScoped illuminatiDAO_TransactionScoped) {
         Illuminati illuminati = new Illuminati();
-        illuminati.setName("Bill Gates 2");
+        illuminati.setName("Mark Zuckerberg");
         illuminati.setCountry("USA");
         illuminati.setDesignation("TransactionScoped_NonTransactional");
         illuminati.setBalance(300000000);
         illuminatiDAO_TransactionScoped.addIlluminati_NonTransactional(illuminati);
-        System.out.println("----------------------------------------------------------");
-        System.out.println(illuminati.getName() + " has joined US!! ");
-        System.out.println("----------------------------------------------------------");
+//        System.out.println("----------------------------------------------------------");
+//        System.out.println(illuminati.getName() + " has joined US!! ");
+//        System.out.println("----------------------------------------------------------");
     }
 
     public static void insertData_TransactionScoped_TransactionalMethod(IlluminatiDAOTransactionScoped illuminatiDAO_TransactionScoped) {
         Illuminati illuminati = new Illuminati();
-        illuminati.setName("Bill Gates 1");
+        illuminati.setName("Bill Gates");
         illuminati.setCountry("USA");
         illuminati.setDesignation("TransactionScoped_Transactional");
         illuminati.setBalance(300000000);
         illuminatiDAO_TransactionScoped.addIlluminati_Transactional(illuminati);
-        System.out.println("----------------------------------------------------------");
-        System.out.println(illuminati.getName() + " has joined US!! ");
-        System.out.println("----------------------------------------------------------");
+//        System.out.println("----------------------------------------------------------");
+//        System.out.println(illuminati.getName() + " has joined US!! ");
+//        System.out.println("----------------------------------------------------------");
     }
 
     private static void listAllIlluminati(IlluminatiDAOTransactionScoped illuminatiDAOTransactionScoped) {
